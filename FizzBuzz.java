bd22f2e317cf8f1d8cd09018b4be30b92186d392
xacaxulu <james@fastmail.se>
2013-06-27 09:02:26 -0600

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,8 @@
+class FizzBuzz {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i%3==0 && i%5==0)  
+        System.out.println("FizzBuzz");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Java FizzBuzz Implementation
+====
